0fdc8471a61bf5bcdbb871c5acf972b1faa4aba5
emonrado <138128501+emonrado@users.noreply.github.com>
2023-06-29 22:34:28 +0900

    Update first.py

--- first.py
+++ first.py
@@ -1,9 +1,8 @@
 import streamlit as st
 st.title("Hello World !")
-st.text('Fixed width text')
-st.slider('Pick a number', 0, 100)
-st.select_slider('Pick a size', ['S', 'M', 'L'])
-st.date_input('Your birthday')
-st.download_button('Download file', data)
+st.text('固定の文字列の表示')
+st.slider('スライダー形式で数字を選択できます。', 0, 100)
+st.select_slider('セレクト形式で値の選択ができます。', ['S', 'M', 'L'])
+st.date_input('日付の選択が可能です。')
 st.camera_input("Take a picture")
 st.color_picker('Pick a color')
