766919f9fe69684d38ace283833d4e835985bdf6
emonrado <138128501+emonrado@users.noreply.github.com>
2023-06-29 22:24:35 +0900

    Create first.py

---
+++ first.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("Hello World !")
+st.text('Fixed width text')
+st.table(data.iloc[0:10])
+st.slider('Pick a number', 0, 100)
+st.select_slider('Pick a size', ['S', 'M', 'L'])
+st.date_input('Your birthday')
+st.download_button('Download file', data)
+st.camera_input("Take a picture")
+st.color_picker('Pick a color')
